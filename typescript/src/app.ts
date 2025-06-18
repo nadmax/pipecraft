1af7b31254accbe725648928af1450fcdcf2cd3a
nadmax <maximilien.nadji@pm.me>
2025-06-18 20:09:34 +0200

    setup tests for each CRUD

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -5,12 +5,12 @@ import { errorHandler } from './middlewares/errorHandler';
 const app = express();
 
 app.use(express.json());
-app.use('/', (_req, res) => {
-    res.json({ success: true, message: 'Hello, World!'});
-});
 app.use('/api/users', userRoutes);
 app.get('/api/health', (_req, res) => {
     res.json({ success: true, message: 'API is running', timestamp: new Date() });
+});
+app.get('/', (_req, res) => {
+    res.json({ success: true, message: 'Hello, World!' });
 });
 app.use((_req, res, next) => {
     res.status(404).json({ success: false, message: 'Oops... Page not found' });
